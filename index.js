1e1f1d42672a3bf0800a8db55da89729a04192c6
davide <davide.dantonio@webeetle.com>
2019-05-15 18:16:42 +0200

    enh

--- index.js
+++ index.js
@@ -3,29 +3,18 @@
 const fp = require('fastify-plugin')
 const typeORM = require('typeorm')
 
-function checkConnectionName (fastify, connection) {
-  if (!fastify.typeorm)
-    return false
-  return fastify.typeorm[connection] ? true : false
-}
-
 async function decorateFastifyInstance (fastify, connections, next) {
   if (connections.length === 0) {
     return next(Error('fastify-typeormdb: no connection info provided'))
   }
 
-  let connection
   try {
-    connection = await typeORM.createConnections(connections)
+    await typeORM.createConnections(connections)
   } catch (e) {
-    return next(Error(e))
+    return next(e)
   }
 
-  fastify.decorate('typeorm', connection)
-  fastify.addHook('onClose', async (instance, done) => {
-    await fastify.typeorm.close()
-    done()
-  })
+  fastify.decorate('typeorm', typeORM)
   next()
 }
 
@@ -34,30 +23,23 @@ function fastifyTypeORM (fastify, options, next) {
     return next(Error('fastify-typeormdb has already been registered'))
   }
 
-  let connections = []
+  let connections = {}
   if (Array.isArray(options)) {
     for (let i = 0; i < options.length; i++) {
       if (!options[i].name) {
         return next(Error('fastify-typeormdb: a connection name must be provided'))
       }
 
-      if (checkConnectionName(fastify, options[i].name)) {
-        return next(Error(`fastify-typeormdb: connection named ${options[i].name} not valid`))
-      }
       connections.push(options[i])
     }
   } else {
-    let name = options.name || 'defaultConn'
-    if (checkConnectionName(fastify, name)) {
-      return next(Error(`fastify-typeormdb: connection named ${name} not valid`))
-    }
     connections.push(options)
   }
 
-  decorateFastifyInstance(fastify, connections, next)
+  decorateFastifyInstance(fastify, Object.values(connections), next)
 }
 
 module.exports = fp(fastifyTypeORM, {
-  fastify: '>=1.1.0',
+  fastify: '>=2.0.0',
   name: 'fastify-typeorm'
 })
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -4,28 +4,38 @@ const Fastify = require('fastify')
 const fastify = Fastify()
 
 fastify.register(plugin, [{
-  "name": "mysql",
+  "name": "fastbee",
   "type": "mysql",
   "host": "localhost",
   "port": 3306,
   "username": "root",
   "password": "fr4g0l4",
   "database": "fastbee"
+},{
+  "name": "ipm2",
+  "type": "mysql",
+  "host": "localhost",
+  "port": 3306,
+  "username": "root",
+  "password": "fr4g0l4",
+  "database": "ipm2"
 }])
 
 fastify.ready(async err => {
   if (err) {
     console.log(err)
   }
-  console.log(fastify.typeorm)
 
-  const users = await fastify.typeorm.get('mysql')
-    .createQueryBuilder()
-    .select('*')
+  let connectionFb
+  try {
+    connectionFb = await fastify.typeorm.getManager('fastbee')
+  } catch (e) {
+    process.exit(1)
+  }
+  const users = await connectionFb.createQueryBuilder()
+    .select('u.username, u.name')
     .from('user', "u")
     .where("u.username = :username", { username: "ddantonio" })
-    .getMany()
-
+    .getRawMany()
   console.log(users)
-  console.log('fastify is running')
 })
